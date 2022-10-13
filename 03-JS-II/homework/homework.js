e99e04bf2204e28b345b4d426499b13ce9abf58e
fg-francogaleano <fg.francogaleano@gmail.com>
2022-10-13 20:08:13 -0300

    commit 4

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -210,7 +210,12 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  
+  if ( numero < 2) return false;
+  if ( numero === 2) return true;
+  for ( var i = 2; i < numero; i++) {
+    if( numero % i === 0 ) return false;
+  }
+  return true
 }
 
 function esVerdadero(valor){
@@ -250,6 +255,13 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+ var resultado = numero
+ 
+  do {
+   numero = numero + 5
+  }
+  while (numero <= (numero + 40))
+  return resultado
 }
 
 
